d73b8e628d07cbdd9cc7134e05be2bfa890da112
Kashish soni <159767573+kashishsoni004@users.noreply.github.com>
2026-01-19 22:55:07 +0530

    update

--- grafna-promethous.sh
+++ grafna-promethous.sh
@@ -137,7 +137,7 @@ route:
 receivers:
   - name: pagerduty
     pagerduty_configs:
-      - routing_key: "4bf6bcd8cb9b4607d047c4b6c2d333d6" #add key pagerduty 
+      - routing_key: "f254273630804307c06297c9b17292ec" #add key pagerduty 
         severity: "critical"
 EOF
 
